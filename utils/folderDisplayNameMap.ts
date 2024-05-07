23f44884ba23d5961328c43a649db2b84c71e9a2
Aoi Kishimoto <20238126@edu.cc.saga-u.ac.jp>
2024-05-08 02:27:57 +0900

    Add wordbooks

--- utils/folderDisplayNameMap.ts
+++ utils/folderDisplayNameMap.ts
@@ -1,4 +1,8 @@
 export const folderDisplayNameMap: { [key: string]: string } = {
   target1900: "ターゲット1900",
+  target1200: "ターゲット1200",
   LEAP: "LEAP",
+  passtan3: "パス単 3級",
+  passtanP2: "パス単 準2級",
+  passtan2: "パス単 2級",
 };
