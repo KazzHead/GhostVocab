60664ab906fea757917da04547f183b66d36f4b3
Aoi Kishimoto <20238126@edu.cc.saga-u.ac.jp>
2024-05-08 16:07:17 +0900

    Change css design

--- pages/events.tsx
+++ pages/events.tsx
@@ -1,20 +1,22 @@
-// import styles from "../styles/index.module.css";
+import styles from "../styles/index.module.css";
 import router from "next/router";
 
 const Events = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <h1>イベント</h1>
       <button onClick={() => router.push(`/`)}>タイトルに戻る</button>
-      <button
-        onClick={() =>
-          router.push(
-            `/study?book=target1900&mode=fillBrackets&start=1&end=230`
-          )
-        }
-      >
-        5月 唐津東高2年 統一テスト ターゲット1900 1～230
-      </button>
+      <div className={styles.buttons}>
+        <button
+          onClick={() =>
+            router.push(
+              `/study?book=target1900&mode=fillBrackets&start=1&end=230`
+            )
+          }
+        >
+          5月 唐津東高2年 統一テスト ターゲット1900 1～230
+        </button>
+      </div>
     </div>
   );
 };
