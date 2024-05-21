6e59954f6488313246092d691706b72acd3e07db
Aoi Kishimoto <20238126@edu.cc.saga-u.ac.jp>
2024-05-22 01:35:44 +0900

    Add Stock3000

--- utils/folderDisplayNameMap.ts
+++ utils/folderDisplayNameMap.ts
@@ -1,5 +1,6 @@
 export const folderDisplayNameMap: { [key: string]: string } = {
   target1900: "ターゲット1900  6訂版",
+  target1400: "ターゲット1400  6訂版",
   target1200: "ターゲット1200  6訂版",
   LEAP: "LEAP",
   passtan4: "パス単 4級 4訂版",
@@ -7,6 +8,7 @@ export const folderDisplayNameMap: { [key: string]: string } = {
   passtanP2: "パス単 準2級 5訂版",
   passtan2: "パス単 2級 5訂版",
   passtanP1: "パス単 準1級 5訂版",
+  stock3000: "Stock3000",
   systan: "システム英単語 5訂版",
   kinhure: "金のフレーズ",
 };
