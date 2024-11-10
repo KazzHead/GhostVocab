d552c89370d99bddd32c85c3e8e605891b60bff2
Kishimoto Aoi <onlybluebookoff@gmail.com>
2024-11-10 15:22:32 +0900

    _documentからタイトルタグ削除。

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -29,7 +29,6 @@ export default class Document extends NextDocument {
             color="#000000"
           />
           <link rel="shortcut icon" href="/favicons/favicon.ico" />{" "}
-          <title>Ghost Vocab</title>
           <meta name="msapplication-TileColor" content="#ffffff" />
           <meta
             name="msapplication-config"
